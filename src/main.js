83be972c8d5c0a432936711d4b6a37298aa650e8
Zach Eliason <eliasonzach@gmail.com>
2020-10-31 17:59:40 -0600

    upload

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import dataset from './dataset.js'
 
 Vue.config.productionTip = false;
+
+let data = {
+  products: dataset,
+}
 
 new Vue({
   router,
